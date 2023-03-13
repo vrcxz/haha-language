f44d0b8778b09661d3cb867b276c9d6c2ddfbe3f
Von Ryan Nogadas <74548381+vonnogadas@users.noreply.github.com>
2023-03-13 15:56:01 +0800

    Initial commit

---
+++ lexer.hpp
@@ -0,0 +1,257 @@
+#ifndef HAHA_LANGUAGE_INTERPRETER_H_
+#define HAHA_LANGUAGE_INTERPRETER_H_
+
+namespace interpreter{
+	
+	struct token {
+		string keyword, name, value;
+		int int_value=0;
+		};
+		
+	struct loop_construct {
+		string line;
+		token new_token;
+		};
+		
+	//Remove this heresy later
+	//vector<token> token_vector;
+	
+	/*
+	- ha = integer
+	- ah = string
+	- haha = print literal
+	- hahaha = print var
+	- hhaa = input
+	- h = newline
+	- aah = system
+	- a = increment/decrement 
+	- haa = if
+	- haahaa = loop
+	- he = end if
+	- hehe = end loop
+	*/
+	
+	vector<token> integer_variables;
+	vector<token> string_variables;
+	vector<loop_construct> loop_vector;
+	bool if_flag_skipper=false;
+	bool loop_flag=false;
+	int loop_number=1;
+	
+	void process_token(token& token_input,string text_line){
+		regex first_word_pattern("^\\w+");
+		regex second_word_pattern("^\\w+\\s+(\\w+)");
+		regex secondAll_word_pattern("^\\w+\\W+([\\w\\W]+)");
+		regex secondAllnumber_word_pattern("^\\w+\\W+(\\d+)");
+		regex second_third_word_pattern("^\\w+\\W+(\\w+)\\W+(\\w+)");
+		regex second_third_math_pattern("^\\w+\\s+(\\+\\+|--)\\s+(\\w+)");
+		regex second_all_word_pattern("^\\w+\\W+(\\w+)\\s+([\\w\\W]+)");
+		regex condition_checker("^\\w+\\s(\\w+)\\s([=|<|>|!])\\s(\\w+)");
+		
+		smatch matched_keyword;
+		
+		
+		
+		if(loop_flag){
+			loop_construct new_loop;
+			new_loop.line = text_line;
+			new_loop.new_token = token_input;
+			loop_vector.push_back(new_loop);
+			}
+		
+		
+		//if
+		regex_search(text_line,matched_keyword,first_word_pattern);
+				if(matched_keyword.str(1)=="he") if_flag_skipper=false;
+				if(if_flag_skipper) return;
+		
+		
+		
+		// int
+		if(token_input.keyword=="ha"){
+			regex_search(text_line,matched_keyword,second_third_word_pattern);
+			token_input.name=matched_keyword.str(1);
+			token_input.value=matched_keyword.str(2);			token_input.int_value=stoi(matched_keyword.str(2));
+			//check first
+			for(auto i : integer_variables){
+				if(i.name==token_input.name){
+					cout<<"Error: An integer variable named '"<<i.name<<"' already exists.";
+					exit(1);
+					}
+				}
+			integer_variables.push_back(token_input);
+			}
+			
+			// string
+		else if(token_input.keyword=="ah"){
+			regex_search(text_line,matched_keyword,second_all_word_pattern);
+			token_input.name=matched_keyword.str(1);
+			token_input.value=matched_keyword.str(2);
+			for(auto i : string_variables){
+				if(i.name==token_input.name){
+					cout<<"Error: A string variable named '"<<i.name<<"' already exists.";
+					exit(1);
+					}
+				}
+			string_variables.push_back(token_input);
+			}
+			
+			//print literal
+		else if(token_input.keyword=="haha"){
+			regex_search(text_line,matched_keyword,secondAll_word_pattern);
+			cout<<matched_keyword.str(1).c_str();
+			}
+			
+		else if(token_input.keyword=="h"){
+			regex_search(text_line,matched_keyword,secondAllnumber_word_pattern);
+			for(int i=0; i<stoi(matched_keyword.str(1)); ++i)
+			cout<<endl;
+			}
+		
+		//print var
+		else if(token_input.keyword=="hahaha"){
+			regex_search(text_line,matched_keyword,second_word_pattern);
+			for(auto i : integer_variables){
+				if(i.name==matched_keyword.str(1)){
+					cout<<i.value;
+					return;
+					}
+				}
+			for(auto i : string_variables){
+					if(i.name==matched_keyword.str(1)){
+					cout<<i.value;
+					return;
+					}
+				}
+			}
+			
+		else if(token_input.keyword=="aah"){
+			regex_search(text_line,matched_keyword,secondAll_word_pattern);
+			system(matched_keyword.str(1).c_str());
+			}
+		
+		else if(token_input.keyword=="a"){
+			regex_search(text_line,matched_keyword,second_third_math_pattern);
+			for(auto &i : integer_variables){
+				if(matched_keyword.str(2)==i.name){
+					
+					if(matched_keyword.str(1)=="++"){
+					++i.int_value;
+					i.value = to_string(i.int_value);
+					}
+					else if(matched_keyword.str(1)=="--"){
+					--i.int_value;
+					i.value = to_string(i.int_value);}
+					else {cout<<"Something's wrong with the operator."; exit(0);}
+					return;
+					}
+				}
+			}
+			
+		else if(token_input.keyword=="haa"){
+			regex_search(text_line,matched_keyword,condition_checker);
+			string x,y;
+			for(auto i : integer_variables){
+				if(i.name==matched_keyword.str(1))
+					x=i.value;
+				if(i.name==matched_keyword.str(3))
+					y=i.value;
+				}
+			for(auto i : string_variables){
+				if(i.name==matched_keyword.str(1))
+					x=i.value;
+				if(i.name==matched_keyword.str(3))
+					y=i.value;
+				}
+				
+				if(matched_keyword.str(2)=="="){
+					if(x!=y) if_flag_skipper=true;
+					return;
+					}
+				else if(matched_keyword.str(2)==">"){
+					if(!(stoi(x)>stoi(y))) if_flag_skipper=true;
+					return;
+					}
+				else if(matched_keyword.str(2)=="<"){
+					if(!(stoi(x)<stoi(y))) if_flag_skipper=true;
+					return;
+					}
+			}
+		
+		else if(token_input.keyword=="hhaa"){
+			regex_search(text_line,matched_keyword,second_word_pattern);
+			
+			for(auto &i : integer_variables){
+				if(i.name==matched_keyword.str(1)){
+					cin>>i.int_value;
+					i.value=to_string(i.int_value);
+					return;
+					}
+				}
+			for(auto &i : string_variables){
+				if(i.name==matched_keyword.str(1)){
+					cin>>i.value;
+					}
+				}
+			}
+		
+		else if(token_input.keyword=="haahaa"){
+			regex_search(text_line,matched_keyword,second_word_pattern);
+			loop_flag=true;
+			for(auto i : integer_variables){
+				if(i.name==matched_keyword.str(1)){
+					loop_number=i.int_value;
+					}
+				}
+			
+			
+			}
+		
+		else if(token_input.keyword=="hehe"){
+			loop_flag=false;
+			if(loop_number<=1) {
+				loop_vector.clear();
+				return;
+				}
+			for(int i=1; i<loop_number; ++i){
+			for(auto& i : loop_vector){
+				if(i.new_token.keyword=="hehe") continue;
+				process_token(i.new_token,i.line);
+					}
+				}
+			loop_vector.clear();
+			}
+		
+		else {}
+	}
+	
+	void read_tokens(string file_name){
+		ifstream file(file_name.c_str());
+		
+		if(file.is_open()){
+			string text_line, token_keyword, token_name, token_value;
+			regex pattern("^\\w+");
+			smatch matched_keyword;
+			
+			while(getline(file,text_line)){
+				token new_token;
+				
+				//text_line+='\n';
+				
+				regex_search(text_line,matched_keyword,pattern);
+				
+				new_token.keyword = matched_keyword.str();
+
+				process_token(new_token,text_line);
+			}
+			
+		} else {
+			cout<<"Can't open file for some reason.";
+		}
+		
+		
+		}
+	
+}
+	
+#endif
---
+++ main.cpp
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////
+// Developer: Von Ryan C. Nogadas  //
+// License: MIT                                     //
+////////////////////////////////////////////////////
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <regex>
+#include <ctime>
+#include <random>
+using namespace std;
+#include "lexer.hpp"
+
+int main()
+{
+	string file_name;
+	cout<<"haha ";
+	cin>>file_name;
+	interpreter::read_tokens(file_name);
+}
